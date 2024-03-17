49f0176a6d2eb7889c7bfbcc723e541b1be2545a
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-03-17 22:25:52 +0500

    Updated files

--- Day7/Question19/main.ts
+++ Day7/Question19/main.ts
@@ -2,7 +2,6 @@
 Question 19: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number
 of people you are inviting to dinner.*/
 
-
 //Using the list from Question 16
 
 let guests: string[] = ["Albert Einstein", "Marie Curie", "Muhammad Ali Jinnah", "John Adams", "Thomas Jefferson", "Queen Elizebeth"]
