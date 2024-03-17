bafe327e35ed86a44fde9d16edc77d8c8db90d92
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-03-17 22:43:10 +0500

    Updated files

--- Day9/Question26/main.js
+++ Day9/Question26/main.js
@@ -1,6 +1,6 @@
 /*Day 9
 Question 26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.*/
-//The ersion where the if block is run
+//The version where the if block is run
 console.log("The version where the if block is run");
 var alien_color = "green";
 if (alien_color == "green") {
@@ -9,7 +9,7 @@ if (alien_color == "green") {
 else {
     console.log("You just earned 10 points!");
 }
-//The Version where the else block is run
+//The version where the else block is run
 console.log("\nThe version where the else block is run");
 alien_color = "red";
 if (alien_color == "green") {
--- Day9/Question26/main.ts
+++ Day9/Question26/main.ts
@@ -1,8 +1,7 @@
 /*Day 9
 Question 26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.*/
 
-
-//The ersion where the if block is run
+//The version where the if block is run
 console.log("The version where the if block is run");
 let alien_color: string = "green";
 
@@ -13,7 +12,7 @@ else{
     console.log("You just earned 10 points!");
 }
 
-//The Version where the else block is run
+//The version where the else block is run
 console.log("\nThe version where the else block is run");
 alien_color = "red"
 
