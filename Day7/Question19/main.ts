394e828d7665b98fb873b30917d75c32e07e906f
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-03-17 22:24:37 +0500

    Updated files

--- Day7/Question19/main.ts
+++ Day7/Question19/main.ts
@@ -3,15 +3,8 @@ Question 19: Dinner Guests: Use one of your programs from Exercises 14 through 1
 of people you are inviting to dinner.*/
 
 
-//Using code from Question 17
+//Using the list from Question 16
 
 let guests: string[] = ["Albert Einstein", "Marie Curie", "Muhammad Ali Jinnah", "John Adams", "Thomas Jefferson", "Queen Elizebeth"]
 
-console.log("Unfortunately, I can only invite two people for dinner.\n");
-
-for (let i = guests.length-1; i > 2; i--){
-    console.log(`Sorry ${guests[i]}, I can't invite you for dinner.`);
-    guests.pop();
-}
-
-console.log(`\nDear ${guests}, you're still invited to dinner.`); //Prints a message indicating the number of people invited to dinner
+console.log(`I am inviting ${guests.length} people to dinner.`);; //Prints a message indicating the number of people invited to dinner
