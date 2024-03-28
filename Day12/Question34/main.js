42ae105c05b19375b3b97a75efbdc088f4380b57
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-03-28 23:08:21 +0500

    Updated files

--- Day12/Question34/main.js
+++ Day12/Question34/main.js
@@ -1,8 +1,7 @@
-"use strict";
 /*Day 12
 Question 34: Pizzas: Share your favorite pizzas and express your love for them.*/
-let pizzas = ["Cheese lover", "Chicken tikka", "Fajita"];
-for (let i = 0; i < pizzas.length; i++) {
-    console.log(`I like ${pizzas[i]} pizza`);
+var pizzas = ["Cheese lover", "Chicken tikka", "Fajita"];
+for (var i = 0; i < pizzas.length; i++) {
+    console.log("I like ".concat(pizzas[i], " pizza"));
 }
 console.log("I really love pizza!");
