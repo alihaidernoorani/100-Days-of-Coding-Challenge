0dedd4833b8c5a246de1f2a1249777bd8f6b6bc3
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-05-01 22:55:48 +0500

    Added Day 34

---
+++ Day34/Question100/main.ts
@@ -0,0 +1,5 @@
+/*Day 34
+Question 100: Use the JavaScript Math object to find the square root of 144.*/
+
+let squareRoot = Math.sqrt(144)
+console.log(squareRoot);
---
+++ Day34/Question101/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Day 34
+Question 101: Generate a random integer between 1 and 10.*/
+function randomInteger() {
+    let variable = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
+    return variable;
+}
+console.log(randomInteger());
