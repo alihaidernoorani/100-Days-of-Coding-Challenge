67596aa8a04865f1a5e0dfe7532946ac1ac44210
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-05-11 23:21:11 +0500

    Updated files

--- Day25/Question75/main.ts
+++ Day25/Question75/main.ts
@@ -1,4 +1,5 @@
-/*Question 75: Compound Assignment Operators:
+/*Day25
+Question 75: Compound Assignment Operators:
 Use compound assignment operators to simplify arithmetic operations. 
 Start with x = 4 and perform a series of operations (addition, subtraction, multiplication, division)
 on x using compound operators.*/
