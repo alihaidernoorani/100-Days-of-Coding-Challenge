947ef1db8398817e455a209583ac26964b672269
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-05-20 19:33:18 +0500

    Updated files

--- Day33/Question98/main.js
+++ Day33/Question98/main.js
@@ -1,8 +1,7 @@
-"use strict";
 /*Day 33
 Question 98: Create a JavaScript snippet that calculates and logs how many days are left until New Year.*/
-let currentDate = new Date(); //Gets the current date
-let currentYear = currentDate.getFullYear();
-let nextYear = currentYear + 1;
-let daysLeft = Math.floor((Number(new Date(nextYear, 0, 1)) - Number(currentDate)) / (24 * 60 * 60 * 1000));
+var currentDate = new Date(); //Gets the current date
+var currentYear = currentDate.getFullYear();
+var nextYear = currentYear + 1;
+var daysLeft = Math.floor((Number(new Date(nextYear, 0, 1)) - Number(currentDate)) / (24 * 60 * 60 * 1000));
 console.log(daysLeft);
--- Day33/Question99/main.js
+++ Day33/Question99/main.js
@@ -1,15 +1,14 @@
-"use strict";
 /*Day 33
 Question 99: Generate a date object representing your next birthday and log it to the console.*/
 function getBirthday(month, day) {
-    const today = new Date();
-    let year = today.getFullYear();
-    const birthday = new Date(year, month - 1, day);
+    var today = new Date();
+    var year = today.getFullYear();
+    var birthday = new Date(year, month - 1, day);
     if (birthday < today) {
         year = year + 1;
         birthday.setFullYear(year);
     }
     return birthday;
 }
-const birthday = getBirthday(1, 21);
+var birthday = getBirthday(1, 21);
 console.log(birthday.toLocaleDateString());
