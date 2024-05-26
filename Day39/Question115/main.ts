1d828288a64f0711e5c77161e0bb897f4e264c41
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-05-26 23:39:35 +0500

    Updated files

---
+++ Day39/Question115/main.ts
@@ -0,0 +1,35 @@
+/*Day 39
+Question 115: Use a switch statement to log the days of the week based on a number (1-7).*/
+
+function getDay(day: number): void{
+    switch(day){
+        case 1:
+            console.log("Sunday");
+            break;
+        case 2:
+            console.log("Monday");
+            break;
+        case 3:
+            console.log("Tuesday");
+            break;
+        case 4:
+            console.log("Wednesday");
+            break;
+        case 5:
+            console.log("Thursday");
+            break;
+        case 6:
+            console.log("Friday");
+            break;
+        case 7:
+            console.log("Saturday");
+            break;
+        default:
+            console.log("Invalid number");
+            break;
+    }
+}
+
+getDay(2);
+getDay(4);
+getDay(7);
---
+++ Day39/Question117/main.js
@@ -0,0 +1,23 @@
+/*Day 39
+Question 117: Implement a switch statement that evaluates an expression and uses the default case if none of the cases match.*/
+function evaluateResult(result) {
+    switch (result) {
+        case 'A':
+            console.log("Excellent!");
+            break;
+        case 'B':
+            console.log("Good!");
+            break;
+        case 'C':
+            console.log("Average");
+            break;
+        case 'F':
+            console.log("Fail");
+            break;
+        default:
+            console.log("Invalid Grade");
+            break;
+    }
+}
+evaluateResult('B'); // This will show the result of case 'B'
+evaluateResult('E'); // This will show the result of default case as none of the cases match
