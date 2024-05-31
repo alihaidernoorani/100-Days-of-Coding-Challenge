6c6b01f6581d18637c5d61a4f7f47f81270f4375
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-05-31 23:26:59 +0500

    Updated files

--- Day44/Question130/main.ts
+++ Day44/Question130/main.ts
@@ -1,5 +1,5 @@
-/*Day 44
-Question 130: Explain how to export a function from one JavaScript file and import it into another file.*/
+/* Day 44
+Question 130: Explain how to export a function from one JavaScript file and import it into another file. */
 
 // In another file where you want to use the add function:
 import {add} from "./mathfunction";
