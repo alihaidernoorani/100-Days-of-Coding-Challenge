ef0595fecfceb5ff25bc709ab103fd300baff9b3
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-06-01 20:48:53 +0500

    Updated files

--- Day44/Question131/main.ts
+++ Day44/Question131/main.ts
@@ -1,5 +1,5 @@
-/*Day 44
-Question 131: Create two modules; one that exports a class, and another that imports the class and creates an instance.*/
+/* Day 44
+Question 131: Create two modules; one that exports a class, and another that imports the class and creates an instance. */
 
 // In another file:
 import {Person} from "./person"
