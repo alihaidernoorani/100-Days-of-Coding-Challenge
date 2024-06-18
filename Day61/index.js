4ec3317c8479d40175d475217c77e764976e7558
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-06-18 23:59:03 +0500

    Updated files

---
+++ Day61/index.js
@@ -0,0 +1,26 @@
+// Q: Replace Function Expressions with arrow functions in the code below:
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+
+
+// Ans:
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+  }
+  
+  ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+  );
