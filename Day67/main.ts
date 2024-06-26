559ce63047fb196e53d91d1eb691718f4f52b24d
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-06-27 00:06:58 +0500

    Updated files

--- Day67/main.ts
+++ Day67/main.ts
@@ -6,6 +6,7 @@ for (let i = 0; i < fruits.length; i++) {
   console.log(`Fruit: ${fruits[i].toUpperCase()}`);
 }
 
+// Type Annotations With Multidimensional Arrays
 let arrayOne: number[] = [1, 2, 3, 4, 5];
 let arrayTwo: string[] = ["A", "B", "C"];
 let arrayThree: (string | number)[] = [1, 2, 3, 4, "A", "B", "C"];
