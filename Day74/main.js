fe1dbc0e22dd0cce3db009a40427957ab429f5bb
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-07-12 02:22:55 +0500

    Updated files

---
+++ Day74/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Debugging
+let age = 22;
+if (age > 20) {
+    console.log("pass");
+}
+else if (age < 20) {
+    console.log("return");
+}
+else {
+    console.log("reboot");
+}
+//# sourceMappingURL=main.js.map
---
+++ Day74/main.ts
@@ -0,0 +1,10 @@
+// Debugging
+
+let age: number = 22;
+if (age > 20) {
+  console.log("pass");
+} else if (age < 20) {
+  console.log("return");
+} else {
+  console.log("reboot");
+}
